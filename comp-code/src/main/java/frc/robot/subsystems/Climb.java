a62c22c6e950d16e6fb8ffb05bf3b450b6fbe45e
Robbie Mones <1170951@pinelakeprep.org>
2019-02-27 12:29:29 -0500

    Enabled climber control

--- comp-code/src/main/java/frc/robot/subsystems/Climb.java
+++ comp-code/src/main/java/frc/robot/subsystems/Climb.java
@@ -26,6 +26,6 @@ public class Climb extends Subsystem {
   public void initDefaultCommand() {
     // Set the default command for a subsystem here.
     // setDefaultCommand(new MySpecialCommand());
-    //setDefaultCommand(new RunClimbers());
+    setDefaultCommand(new RunClimbers());
   }
 }
